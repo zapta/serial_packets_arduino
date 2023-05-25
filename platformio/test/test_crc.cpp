648aca5e858f5b0f4cd917c00680a67af3b653d5
Zapta <zapta@users.noreply.github.com>
2023-05-25 12:29:50 -0700

    Completed the CRC unit test.

--- platformio/test/test_crc.cpp
+++ platformio/test/test_crc.cpp
@@ -1,35 +1,18 @@
 // Uring test of the packet crc function.
 
-// For test example see here
+// NOTE: As for May 2023, the test runs only in verbose mode and
+// does not run when using the platformio hourglass test icron.
+
+// Based on this example.
 // https://github.com/platformio/platformio-examples/blob/develop/unit-testing/calculator/test/test_embedded/test_calculator.cpp
 
 #include <Arduino.h>
 #include <unity.h>
+
 #include "packet_crc.h"
 
-#define LED PC13
-
-// void setUp(void) {
-//   // set stuff up here
-// }
-
-// void tearDown(void) {
-//   // clean stuff up here
-// }
-
-// void test_calculator_addition(void) {
-//   // TEST_ASSERT_EQUAL(32, calc.add(25, 7));
-// }
-
-// void test_calculator_subtraction(void) {
-//   // TEST_ASSERT_EQUAL(20, calc.sub(23, 3));
-// }
-
-// void test_calculator_multiplication(void) {
-//   // TEST_ASSERT_EQUAL(50, calc.mul(25, 2));
-//      TEST_ASSERT_EQUAL(1, 2);
-
-// }
+// For STM32 'black pill'.
+#define BUILTIN_LED PC13
 
 void test_empty_data(void) {
   const uint8_t data[] = {};
@@ -37,25 +20,38 @@ void test_empty_data(void) {
   TEST_ASSERT_EQUAL(0xffff, crc);
 }
 
-void setup() {
-  // pinMode(LED, OUTPUT);
-  // digitalWrite(LED, LOW);
+// Per https://srecord.sourceforge.net/crc16-ccitt.html#results this is
+// the wrong result, but it's a common mistake, and compatible with the
+// the Python library we use.
+void test_data1(void) {
+  const uint8_t data[] = {0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39};
+  const uint16_t crc = packet_crc::gen_crc16(data, sizeof(data));
+  TEST_ASSERT_EQUAL(0x29b1, crc);
+}
 
-  // NOTE!!! Wait for >2 secs
-  // if board doesn't support software reset via Serial.DTR/RTS
+void test_data2(void) {
+  const uint8_t data[] = {0x01, 0x00, 0x00, 0x00, 0x07, 0x14,
+                          0xc8, 0x00, 0x00, 0x04, 0xd2};
+  const uint16_t crc = packet_crc::gen_crc16(data, sizeof(data));
+  TEST_ASSERT_EQUAL(0x1f49, crc);
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);
+
+  // Time for the USB/CDC serial to stabalizes.
   delay(2000);
 
   UNITY_BEGIN();
-  // RUN_TEST(test_calculator_addition);
-  // RUN_TEST(test_calculator_subtraction);
-  // RUN_TEST(test_calculator_multiplication);
   RUN_TEST(test_empty_data);
+  RUN_TEST(test_data1);
+  RUN_TEST(test_data2);
   UNITY_END();
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
+  digitalWrite(BUILTIN_LED, HIGH);
   delay(500);
-  digitalWrite(LED, LOW);
+  digitalWrite(BUILTIN_LED, LOW);
   delay(500);
 }
