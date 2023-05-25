fe1599fb523c976751488ac622d016c4d7620221
Zapta <zapta@users.noreply.github.com>
2023-05-25 14:19:58 -0700

    More progress on the packet data tests. Incomplete.

--- platformio/test/test_packet_crc/test_packet_crc.cpp
+++ platformio/test/test_packet_crc/test_packet_crc.cpp
@@ -11,7 +11,7 @@
 // For STM32 'black pill'.
 #define BUILTIN_LED PC13
 
-void test_empty_data(void) {
+void test_empty_data() {
   const uint8_t data[] = {};
   const uint16_t crc = packet_crc::gen_crc16(data, sizeof(data));
   TEST_ASSERT_EQUAL(0xffff, crc);
@@ -20,13 +20,13 @@ void test_empty_data(void) {
 // Per https://srecord.sourceforge.net/crc16-ccitt.html#results this is
 // the wrong result, but it's a common mistake, and compatible with the
 // the Python library we use.
-void test_data1(void) {
+void test_data1() {
   const uint8_t data[] = {0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39};
   const uint16_t crc = packet_crc::gen_crc16(data, sizeof(data));
   TEST_ASSERT_EQUAL_HEX32(0x29b1, crc);
 }
 
-void test_data2(void) {
+void test_data2() {
   const uint8_t data[] = {0x01, 0x00, 0x00, 0x00, 0x07, 0x14,
                           0xc8, 0x00, 0x00, 0x04, 0xd2};
   const uint16_t crc = packet_crc::gen_crc16(data, sizeof(data));
@@ -36,7 +36,7 @@ void test_data2(void) {
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);
 
-  // Time for the USB/CDC serial to stabalizes.
+  // Time for the USB/CDC serial to stabalize.
   delay(2000);
 
   UNITY_BEGIN();
--- platformio/test/test_packet_data/test_packet_data.cpp
+++ platformio/test/test_packet_data/test_packet_data.cpp
@@ -5,25 +5,37 @@
 
 #include <Arduino.h>
 #include <unity.h>
+#include <vector>
 
 #include "packet_data.h"
 
 // For STM32 'black pill'.
 #define BUILTIN_LED PC13
 
-void assert_data_equals(const PacketData& data, const uint8_t* expected,
-                        int expected_size) {
+// Side affect of reseting the reading.
+void assert_data_equals(const PacketData& data, const std::vector<uint8_t> expected) {
   data.reset_reading();
-  for (int i = 0; i < expected_size; i++) {
+  for (int i = 0; i < expected.size(); i++) {
     const uint8_t b = data.read_uint8();
     TEST_ASSERT_FALSE(data.read_errors());
-    TEST_ASSERT_EQUAL_HEX8(b, expected[i]);
+    TEST_ASSERT_EQUAL_HEX8(b, expected.at(i));
     TEST_ASSERT_FALSE(data.read_errors());
   }
   TEST_ASSERT_TRUE(data.all_read_ok());
+  data.reset_reading();
 }
 
-void test_constructor(void) {
+// Side affect of reseting the reading.
+void populate_data(const std::vector<uint8_t> bytes, PacketData* data) {
+  data->clear();
+  for (int i = 0; i < bytes.size(); i++) {
+    data->write_uint8(bytes.at(i));
+  }
+  TEST_ASSERT_FALSE(data->write_errors());
+  assert_data_equals(*data, bytes);
+}
+
+void test_constructor() {
   PacketData d;
   TEST_ASSERT_EQUAL(0, d.size());
   TEST_ASSERT_EQUAL(0, d.bytes_read());
@@ -34,25 +46,37 @@ void test_constructor(void) {
   TEST_ASSERT_TRUE(d.all_read_ok());
 }
 
-void test_write_uint8(void) {
+void test_write_uint8() {
   PacketData d;
   d.write_uint8(0x02);
   TEST_ASSERT_EQUAL(1, d.size());
   TEST_ASSERT_EQUAL(0, d.bytes_read());
   TEST_ASSERT_EQUAL(1, d.bytes_left_to_read());
-  const uint8_t expected[] = {0x02};
-  assert_data_equals(d, expected, sizeof(expected));
+  assert_data_equals(d, {0x02});
+}
+
+void test_read_uint8() {
+  PacketData d;
+  populate_data({0x11, 0x22, 0x33}, &d);
+  const uint8_t v1 = d.read_uint8();
+  const uint8_t v2 = d.read_uint8();
+  TEST_ASSERT_FALSE(d.read_errors());
+  TEST_ASSERT_EQUAL(v1, 0x11);
+  TEST_ASSERT_EQUAL(v2, 0x22);
+  TEST_ASSERT_EQUAL(2, d.bytes_read());
+  TEST_ASSERT_EQUAL(1, d.bytes_left_to_read());
 }
 
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);
 
-  // Time for the USB/CDC serial to stabalizes.
+  // Time for the USB/CDC serial to stabalize.
   delay(2000);
 
   UNITY_BEGIN();
   RUN_TEST(test_constructor);
   RUN_TEST(test_write_uint8);
+  RUN_TEST(test_read_uint8);
   UNITY_END();
 }
 
